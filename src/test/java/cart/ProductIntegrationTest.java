a0496c912b87c1f8c01db0cb0fcf457ffbfc2e67
rheehyerin <toohappy2122@gmail.com>
2023-04-09 18:59:10 +0900

    스프링카트 Step1 (#8)
    * feat(상품 조회) : 1차 초안
    
    * feat(상품 조회) : 1차 초안 수정
    
    * feat(상품 조회) : 상품목록 페이지 완성
    
    * feat(카트): admin init
    
    * feat(상품 crud) : 상품 추가/삭제/업데이트 완료

--- src/test/java/cart/ProductIntegrationTest.java
+++ src/test/java/cart/ProductIntegrationTest.java
@@ -1,5 +1,6 @@
 package cart;
 
+import cart.domain.Product;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductIntegrationTest {
-
     @LocalServerPort
     private int port;
 
@@ -27,11 +27,86 @@ public class ProductIntegrationTest {
         var result = given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
-                .get("/products")
+                .get("/")
                 .then()
                 .extract();
 
         assertThat(result.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
+    @Test
+    public void 상품_추가_테스트() {
+        Product product = new Product(
+                    "치킨",
+                    "img/chicken.jpg",
+                    15000
+                     );
+
+        var result = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .body(product)
+                .post("product/create")
+                .then()
+                .extract();
+
+        assertThat(result.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+
+    @Test
+    public void 상품_정보_업데이트_테스트() {
+        Product initProduct = new Product(
+                "치킨",
+                "img/chicken.jpg",
+                15000
+        );
+        Product product = new Product(
+                Long.valueOf("1"),
+                "짬뽕",
+                "https://ak-d.tripcdn.com/images/1mi3j2224udq9w7pcE08F_W_670_10000.jpg?proc=source/trip",
+                13000
+        );
+
+        given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .body(initProduct)
+                .post("product/create")
+                .then();
+
+
+        var result = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .body(product)
+                .put("product/update")
+                .then()
+                .extract();
+        assertThat(result.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void 상품_삭제_테스트() {
+        Product initProduct = new Product(
+                "치킨",
+                "img/chicken.jpg",
+                15000
+        );
+        given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .body(initProduct)
+                .post("product/create")
+                .then();
+
+        var result = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .params("id",Long.valueOf("1"))
+                .delete("product/delete")
+                .then()
+                .extract();
+        assertThat(result.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
 }
