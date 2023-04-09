bc997388ffd09dd9ac3deb140d6b0ebfad00939e
justsin1990 <89959637+justsin1990@users.noreply.github.com>
2023-04-09 16:09:51 +0900

    스프링카트기능구현

---
+++ src/main/java/cart/controller/AdminController.java
@@ -0,0 +1,25 @@
+package cart.controller;
+
+import cart.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+@RequiredArgsConstructor
+public class AdminController {
+    private final ProductService productService;
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        model.addAttribute(
+                "products",
+                productService.getProducts()
+        );
+        return "/admin";
+    }
+
+}
---
+++ src/main/java/cart/service/ProductService.java
@@ -0,0 +1,24 @@
+package cart.service;
+
+import cart.domain.Product;
+import cart.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    public void addProduct(Product product) {
+        productRepository.insert(product);
+    }
+    public void deleteProduct(Long id) {
+        productRepository.delete(id);
+    }
+}
