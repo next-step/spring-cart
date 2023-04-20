5b07851f9d767153f5fc220b1a2c20b164a1d6cc
jade.cyan <cyanjd1226@gmail.com>
2023-04-20 21:26:33 +0900

    클래스명 변경

--- src/main/java/cart/controller/CartViewController.java
+++ src/main/java/cart/controller/CartViewController.java
@@ -3,7 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class ViewController {
+public class CartViewController {
     @GetMapping("/cart")
     public String cart() {
         return "cart";
