78d21d6b788ab8193907525c0aef964668801d96
KAI-20200079\kakaopaysec <kai.bk.study@gmail.com>
2023-04-21 16:55:32 +0900

    PR리뷰 최종commit

--- src/test/java/cart/admin/api/AdminControllerTest.java
+++ src/test/java/cart/admin/api/AdminControllerTest.java
@@ -1,10 +1,8 @@
 package cart.admin.api;
 
 import cart.RestAssuredApiSteps;
-import cart.ProductAdminService;
 import cart.dao.ProductDao;
 import cart.domain.Product;
-import cart.controller.ProductAdminController;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,12 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -56,15 +48,10 @@ public class AdminControllerTest {
     @DisplayName("상품 정보 업데이트(가격) 테스트")
     @Test
     void putAdminProduct() {
-
         Product product = adminProductDao.insertProduct(new Product("iPhone15Pro", "http://wwww.apple.co.kr/index.html", 120000));
-
         product.setPrice(150000);
 
         var result = productRestAssuredApiSteps.updateUrlWithBody("/admin/product", product);
-
         assertThat(result.statusCode()).isEqualTo(HttpStatus.ACCEPTED.value());
     }
-
-
 }
