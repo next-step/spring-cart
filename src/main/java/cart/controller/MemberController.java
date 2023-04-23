6d7af6873ae3918e0aabe95a5625123d8a4c9eec
hyerinrhee <toohappy2122@gmail.com>
2023-04-24 08:55:40 +0900

    feat(사용자) : 사용자 설정 페이지 로딩(settings)

--- src/main/java/cart/controller/MemberController.java
+++ src/main/java/cart/controller/MemberController.java
@@ -1,4 +1,21 @@
 package cart.controller;
 
+import cart.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+    @GetMapping("/settings")
+    public String getMembers(Model model) {
+        model.addAttribute(
+                "members",
+                memberService.getMembers()
+        );
+        return "/settings";
+    }
 }
