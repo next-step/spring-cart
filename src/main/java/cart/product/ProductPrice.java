2fa19bd0cb9e2c83581e4e7ab12cd0d5064a4ac3
Hyeon9mak <jinha3507@gmail.com>
2023-04-25 23:19:29 +0900

    feat: 상품 가격 구현

---
+++ src/main/java/cart/product/ProductPrice.java
@@ -0,0 +1,40 @@
+package cart.product;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class ProductPrice {
+    private final BigDecimal value;
+
+    public ProductPrice(Long value) {
+        this(new BigDecimal(value));
+    }
+
+    public ProductPrice(BigDecimal value) {
+        this.value = value;
+        validatePositive(this.value);
+    }
+
+    private void validatePositive(BigDecimal price) {
+        if (price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("상품 가격은 0원 이상이어야 합니다.");
+        }
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductPrice that = (ProductPrice) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
