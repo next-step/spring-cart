0b2083670bc01efd0c047089c8537e2d579f6388
hjhello423 <hjhello423@gmail.com>
2023-05-05 01:29:25 +0900

    test: 테스트 환경 세팅

---
+++ src/test/java/cart/fixture/AssuredTest.java
@@ -0,0 +1,22 @@
+package cart.fixture;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestExecutionListeners;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestExecutionListeners(value = {DatabaseCleanup.class,}, mergeMode =
+        TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
+public class AssuredTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
+}
