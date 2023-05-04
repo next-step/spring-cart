77cace1ae8a046602c7f55c01c5921a930d90589
hjhello423 <hjhello423@gmail.com>
2023-05-05 01:11:25 +0900

    chore: 환경 세팅

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE PRODUCT
+(
+    id         INT            NOT NULL AUTO_INCREMENT,
+    name       VARCHAR(50)    NOT NULL,
+    image      VARCHAR(200)    NOT NULL,
+    price      DECIMAL(10, 2) NOT NULL,
+    created_at DATETIME       NOT NULL default current_timestamp,
+    PRIMARY KEY (id)
+);
+
