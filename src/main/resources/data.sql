e4910f48b022feb8676ad088fa6f80c40bf1cdc0
hjhello423 <hjhello423@gmail.com>
2023-05-05 01:48:47 +0900

    chore: 테스트용 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,11 @@ CREATE TABLE PRODUCT
 (
     id         INT            NOT NULL AUTO_INCREMENT,
     name       VARCHAR(50)    NOT NULL,
-    image      VARCHAR(200)    NOT NULL,
+    image      VARCHAR(200)   NOT NULL,
     price      DECIMAL(10, 2) NOT NULL,
     created_at DATETIME       NOT NULL default current_timestamp,
     PRIMARY KEY (id)
 );
 
+INSERT INTO product (name, image, price)
+VALUES ('떡볶이', '/images/tteokbokki.jpg', 5000);
