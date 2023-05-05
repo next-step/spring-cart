ac333ab1a32a5de02441ddd2878ef23de4c63d7d
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-05-05 17:51:24 +0900

    feat: 상품 조회 결과 뷰 페이지와 연동

--- src/main/java/cart/product/web/ProductController.java
+++ src/main/java/cart/product/web/ProductController.java
@@ -6,12 +6,22 @@ import cart.product.web.dto.ProductInfo;
 import cart.product.web.dto.UpdateProduct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @Slf4j
 public class ProductController {
+
+    @GetMapping("/")
+    public ModelAndView showIndexView() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        modelAndView.addObject("products", readProducts());
+        return modelAndView;
+    }
 
     @PostMapping("/product")
     public CreateProduct.Response createProduct(
@@ -29,13 +39,35 @@ public class ProductController {
     @GetMapping("/product/list")
     public List<ProductInfo> readProducts() {
 
-        return null;
+        List<ProductInfo> products = new ArrayList<>();
+        products.add(ProductInfo.builder()
+                .id(1L)
+                .name("치킨")
+                .image("/images/chicken.jpeg")
+                .price(10000)
+                .build());
+
+        products.add(ProductInfo.builder()
+                .id(2L)
+                .name("샐러드")
+                .image("/images/salad.jpeg")
+                .price(2000)
+                .build());
+
+        products.add(ProductInfo.builder()
+                .id(3L)
+                .name("피자")
+                .image("/images/pizza.jpeg")
+                .price(20000)
+                .build());
+
+        return products;
     }
 
     @PostMapping("/product/update")
     public UpdateProduct.Response updateProduct(
             @RequestBody UpdateProduct.Request request
-            ) {
+    ) {
 
         return null;
     }
