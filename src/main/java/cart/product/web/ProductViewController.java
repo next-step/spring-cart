71c9f44956bd54e420a705c17a2fe13aa89ba6f9
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-05-08 22:08:20 +0900

    refactor: 클래스명 변경

--- src/main/java/cart/product/web/ProductViewController.java
+++ src/main/java/cart/product/web/ProductViewController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
-public class ProductWebController {
+public class ProductViewController {
 
     private final ProductApiController productApiController;
 
