b5896b63cad6b7ad9366ebd5fbd0559fa7ddcc5a
JangHyeonJun2 <epfvkdlxj@naver.com>
2023-05-10 00:54:09 +0900

    update: 리렌더링 방지 및 restController 피드백

--- src/main/java/cart/admin/controller/AdminController.java
+++ src/main/java/cart/admin/controller/AdminController.java
@@ -2,42 +2,44 @@ package cart.admin.controller;
 
 import cart.product.dto.request.ProductRequest;
 import cart.product.service.ProductService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 
-@RequiredArgsConstructor
 @RequestMapping("/admin")
-@Controller
+@RestController
 public class AdminController {
     private final ProductService productService;
+    private final ModelAndView modelAndView = new ModelAndView();
+
+    public AdminController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("")
-    public String findAll(Model model) {
+    public ModelAndView findAll(Model model) {
         model.addAttribute("products", productService.findAll());
-        return "admin";
+        modelAndView.setViewName("admin");
+        return modelAndView;
     }
 
     @PostMapping("/create-product")
-    public String insertProduct(Model model, @RequestBody ProductRequest productRequest) {
+    public void insertProduct(Model model, @RequestBody ProductRequest productRequest) {
         productService.insert(productRequest);
         model.addAttribute("products", productService.findAll()); // 최신화 시켜주기
-        return "admin";
     }
 
     @PutMapping("/update-product/{id}")
-    public String updateProduct(Model model, @PathVariable Long id, @RequestBody ProductRequest productRequest) {
+    public void updateProduct(Model model, @PathVariable Long id, @RequestBody ProductRequest productRequest) {
         productService.update(id, productRequest);
         model.addAttribute("products", productService.findAll()); // 최신화 시켜주기
-        return "admin";
     }
 
     @DeleteMapping("/delete-product/{id}")
-    public String deleteProduct(Model model, @PathVariable Long id) {
+    public void deleteProduct(Model model, @PathVariable Long id) {
         productService.delete(id);
         model.addAttribute("products", productService.findAll()); // 최신화 시켜주기
-        return "admin";
     }
 
 
