69cadb65e3f33e017610150433cfd995f2e46de2
00hongjun <00hongjun@gmail.com>
2023-05-10 15:42:03 +0900

    test: 테이블 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,5 +8,26 @@ CREATE TABLE PRODUCT
     PRIMARY KEY (id)
 );
 
+CREATE TABLE MEMBER
+(
+    id         INT          NOT NULL AUTO_INCREMENT,
+    email      VARCHAR(50)  NOT NULL,
+    password   VARCHAR(100) NOT NULL,
+    created_at DATETIME     NOT NULL default current_timestamp,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE CART
+(
+    id         INT      NOT NULL AUTO_INCREMENT,
+    member_id  INT      NOT NULL,
+    product_id INT      NOT NULL,
+    created_at DATETIME NOT NULL default current_timestamp,
+    PRIMARY KEY (id)
+);
+
 INSERT INTO product (name, image, price)
 VALUES ('떡볶이', '/images/tteokbokki.jpg', 5000);
+
+INSERT INTO member (email, password)
+VALUES ('user', '1111');
