b69083f70a34b5bbd81739a57c57e57c9d37a2d7
gmelon <hsh1769@naver.com>
2023-05-12 17:16:10 +0900

    test: ProductApiControllerTest 에서 db 값 반영 여부까지 검증하도록 수정

--- src/test/java/cart/web/ProductApiControllerTest.java
+++ src/test/java/cart/web/ProductApiControllerTest.java
@@ -14,7 +14,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @Sql(scripts = "classpath:schema.sql")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -33,32 +38,51 @@ class ProductApiControllerTest {
 
     @Test
     void save() {
+        // given
         ProductSaveRequestDto requestDto = new ProductSaveRequestDto("상품A", "image.com/imageA", 10000);
-        RestAssured.given()
+
+        // when
+        Long savedProductId = RestAssured.given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(requestDto)
                 .when().post("/api/v1/products")
+
+        // then
                 .then()
-                .statusCode(HttpStatus.CREATED.value());
+                .statusCode(HttpStatus.CREATED.value()).extract().as(Long.class);
+
+        Product foundProduct = assertDoesNotThrow(() -> productDao.findById(savedProductId).get());
+        assertThat(foundProduct.getName()).isEqualTo("상품A");
+        assertThat(foundProduct.getImageUrl()).isEqualTo("image.com/imageA");
+        assertThat(foundProduct.getPrice()).isEqualTo(10000);
     }
 
     @Test
     void update_valid() {
+        // given
         Product givenProduct = Product.builder()
                 .name("상품A")
                 .imageUrl("image.com/imageA")
                 .price(10000)
                 .build();
         Product savedProduct = productDao.insert(givenProduct);
-
         ProductUpdateRequestDto requestDto = new ProductUpdateRequestDto("상품B", "image.com/imageB", 20000);
-        RestAssured.given()
+
+        // when
+        Long updatedProductId = RestAssured.given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(requestDto)
                 .when().put("/api/v1/products/" + savedProduct.getId())
+
+        // then
                 .then()
                 .statusCode(HttpStatus.OK.value())
-                .body(is(savedProduct.getId().toString()));
+                .body(is(savedProduct.getId().toString())).extract().as(Long.class);
+
+        Product foundProduct = assertDoesNotThrow(() -> productDao.findById(updatedProductId).get());
+        assertThat(foundProduct.getName()).isEqualTo("상품B");
+        assertThat(foundProduct.getImageUrl()).isEqualTo("image.com/imageB");
+        assertThat(foundProduct.getPrice()).isEqualTo(20000);
     }
 
     @Test
@@ -73,6 +97,7 @@ class ProductApiControllerTest {
 
     @Test
     void delete_valid() {
+        // given
         Product givenProduct = Product.builder()
                 .name("상품A")
                 .imageUrl("image.com/imageA")
@@ -80,13 +105,18 @@ class ProductApiControllerTest {
                 .build();
         Product savedProduct = productDao.insert(givenProduct);
 
-        RestAssured.given()
+        // when
+        Long deletedProductId = RestAssured.given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().delete("/api/v1/products/" + savedProduct.getId())
+
+        // then
                 .then()
                 .statusCode(HttpStatus.OK.value())
-                .body(is(savedProduct.getId().toString()));
+                .body(is(savedProduct.getId().toString())).extract().as(Long.class);
 
+        assertThatThrownBy(() -> productDao.findById(deletedProductId).get())
+                .isInstanceOf(NoSuchElementException.class);
     }
 
     @Test
