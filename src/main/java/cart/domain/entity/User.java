3ba0f5b00fd82cef9b95ae6f63d2428385fde2f6
yeeun-song <yeeun.song@toss.im>
2023-05-12 20:53:17 +0900

    Feat: User entity 생성

---
+++ src/main/java/cart/domain/entity/User.java
@@ -0,0 +1,27 @@
+package cart.domain.entity;
+
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+
+    public User() {}
+
+    public User(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,3 +13,15 @@ values ('치킨', 20000, 'https://i.namu.wiki/i/YVm0x8WHfLBtSyejD01_GTV1ITfWOJ-X
 ('샐러드', 18000, 'https://i.namu.wiki/i/_cLeYQStb5Xf8h6vC4ZtRoGFvsF1-rb9wqUMJ9pS510fYeJCGOnh-CQ0A0n9YNHbZ5Roi4WX3NB5RTPdYmhJyg.webp'),
 ('피자', 30000, 'https://handmadepizza.co.kr/uploadFiles/TB_MENU_MAIN/20230403163710691_93d0900607594ee2bfdbd6da4330fbfb.png');
 
+CREATE TABLE USERS (
+     id          INT         NOT NULL AUTO_INCREMENT,
+     email     VARCHAR(100) NOT NULL,
+     password     VARCHAR(100) NOT NULL,
+     created_at  DATETIME    NOT NULL default current_timestamp,
+     updated_at  DATETIME    NOT NULL default current_timestamp,
+     PRIMARY KEY (id)
+);
+
+insert into USERS(email, password)
+values ('a@a.com', 'password1'),
+       ('b@b.com', 'password2');
