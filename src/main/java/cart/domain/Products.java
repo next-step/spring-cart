5bb77a9b485c80693bd98e4e3a5d8c27f0ae57b3
chacha <peony@bluehole.net>
2023-05-14 21:58:46 +0900

    feat Products 도메인 추가

---
+++ src/main/java/cart/domain/Products.java
@@ -0,0 +1,11 @@
+package cart.domain;
+
+import java.util.List;
+
+public class Products {
+	private final List<Product> products;
+
+	public Products(List<Product> products) {
+		this.products = products;
+	}
+}
