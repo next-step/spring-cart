6a3b4201a87bd6903b4ba71b4a9ba6d7aeba6308
haileykim2014 <haileykim2014@gmail.com>
2023-05-15 04:22:40 +0900

    feature : admin page

---
+++ src/main/java/cart/AdminController.java
@@ -0,0 +1,22 @@
+package cart;
+
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+  private final ProductService productService;
+
+  public AdminController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @GetMapping("/admin")
+  public String getAll(Model model) {
+    List<Product> products = productService.getAll();
+    model.addAttribute("products", products);
+    return "admin";
+  }
+}
