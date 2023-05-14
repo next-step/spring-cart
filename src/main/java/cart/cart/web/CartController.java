e4f9ffa2405cca43536adca1e1b6a2407adf1206
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-05-15 00:48:15 +0900

    feat: ArgumentResolver(AuthenticationPrincipal)

--- src/main/java/cart/cart/web/CartController.java
+++ src/main/java/cart/cart/web/CartController.java
@@ -1,21 +1,16 @@
 package cart.cart.web;
 
+import cart.auth.AuthPrincipal;
 import cart.cart.domain.service.CartService;
-import cart.auth.AuthInfo;
 import cart.cart.web.dto.CreateAuthRequest;
 import cart.cart.web.dto.CreateCart;
 import cart.cart.web.dto.ReadMemberCarts;
-import cart.member.domain.dto.MemberDto;
+import cart.global.config.argumentResolver.AuthenticationPrincipal;
 import cart.member.domain.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.naming.AuthenticationException;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Base64;
 
 
 @RestController
@@ -26,30 +21,9 @@ public class CartController {
     private final MemberService memberService;
 
     @GetMapping(value = "/carts/auth")
-    public ReadMemberCarts.Response readMemberCartsInAuth(
-            HttpServletRequest request) throws AuthenticationException {
-        MemberDto memberDto = getMemberFromAuth(request);
-        return readMemberCarts(memberDto.getId());
+    public ReadMemberCarts.Response readMemberCartsInAuth(@AuthenticationPrincipal AuthPrincipal authPrincipal) {
+        return readMemberCarts(authPrincipal.getMemberId());
     }
-
-    private MemberDto getMemberFromAuth(HttpServletRequest request) throws AuthenticationException {
-        String authorization = request.getHeader("Authorization");
-        if (!StringUtils.hasText(authorization)) {
-            throw new AuthenticationException("인증 정보가 없습니다");
-        }
-        AuthInfo authInfo = convert(authorization);
-        return memberService.getMember(authInfo.getUsername(), authInfo.getPassword());
-    }
-
-    private AuthInfo convert(String basicAuthLiteral) {
-        String authLiteral = basicAuthLiteral.replace("Basic ", "");
-        String decodedString
-                = new String(Base64.getDecoder().decode(authLiteral.getBytes()));
-
-        String[] authArray = decodedString.split(":");
-        return new AuthInfo(authArray[0], authArray[1]);
-    }
-
 
     @GetMapping("/carts")
     public ReadMemberCarts.Response readMemberCarts(@RequestParam Long memberId) {
@@ -59,10 +33,9 @@ public class CartController {
 
     @PostMapping("/carts/auth")
     public CreateCart.Response createCart(@RequestBody CreateAuthRequest request,
-                                          HttpServletRequest httpServletRequest) throws AuthenticationException {
-        MemberDto memberDto = getMemberFromAuth(httpServletRequest);
+                                          @AuthenticationPrincipal AuthPrincipal authPrincipal){
         return CreateCart.Response.of(
-                cartService.addCart(memberDto.getId(), request.getProductId()));
+                cartService.addCart(authPrincipal.getMemberId(), request.getProductId()));
     }
 
     @PostMapping("/carts")
--- src/main/java/cart/global/config/WebMvcConfiguration.java
+++ src/main/java/cart/global/config/WebMvcConfiguration.java
@@ -1,15 +1,29 @@
 package cart.global.config;
 
+import cart.global.config.argumentResolver.AuthenticationPrincipalArgumentResolver;
 import cart.global.interceptor.LoginInterceptor;
+import cart.member.domain.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.*;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    private final MemberService memberService;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/carts/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthenticationPrincipalArgumentResolver(memberService));
     }
 }
