32bcdb4d4bf39a52fbcae903e11e992f2af7a607
chacha <peony@bluehole.net>
2023-05-14 22:03:34 +0900

    feat ProductService 추가

---
+++ src/main/java/cart/service/ProductService.java
@@ -0,0 +1,55 @@
+package cart.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import cart.domain.Product;
+import cart.dto.ProductRequestDto;
+import cart.dto.ProductResponseDto;
+import cart.dto.ProductUpdateRequest;
+import cart.dto.ProductsResponseDto;
+import cart.exception.ErrorType;
+import cart.exception.ServiceException;
+import cart.repository.ProductRepository;
+
+@Service
+public class ProductService {
+	private final ProductRepository productRepository;
+
+	public ProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
+	@Transactional
+	public ProductResponseDto create(ProductRequestDto productRequestDto) {
+		Product product = productRequestDto.toProduct();
+		return ProductResponseDto.from(productRepository.save(product));
+	}
+
+	@Transactional
+	public ProductResponseDto update(Long id, ProductUpdateRequest productUpdateRequest) {
+		Product product = findProduct(id);
+		product.setName(productUpdateRequest.getName());
+		product.setPrice(productUpdateRequest.getPrice());
+		product.setImage(productUpdateRequest.getImage());
+		productRepository.update(product);
+		return ProductResponseDto.from(product);
+	}
+
+	private Product findProduct(Long id) {
+		System.out.println(productRepository.findById(id));
+		return productRepository.findById(id)
+			.orElseThrow(() -> new ServiceException(ErrorType.PRODUCT_NOT_FOUND));
+	}
+
+	@Transactional
+	public void deleteProduct(Long id) {
+		Product product = findProduct(id);
+		productRepository.delete(product);
+	}
+
+	public ProductsResponseDto findAll() {
+		return ProductsResponseDto.from(productRepository.findAll());
+	}
+
+}
