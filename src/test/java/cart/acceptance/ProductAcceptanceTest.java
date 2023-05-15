4fef3b75cddfe427149a52353726f52bec743a4a
jisu <jisuuoo96@gmail.com>
2023-05-15 17:29:23 +0900

    test: 상품 생성 기능 인수테스트 작성

--- src/test/java/cart/acceptance/ProductAcceptanceTest.java
+++ src/test/java/cart/acceptance/ProductAcceptanceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+
+import static cart.acceptance.ProductSteps.상품_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 import static cart.acceptance.ProductSteps.상품_목록_요청;
 
@@ -16,15 +18,22 @@ class ProductAcceptanceTest {
     @Test
     @DisplayName("/products 로 접근할 경우 상품 목록 조회에 성공한다.")
     void getProductsTest() {
-        ExtractableResponse<Response> response = 상품_목록_요청("/products");
+        var response = 상품_목록_요청("/products");
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
     @Test
     @DisplayName("/products 와 다른 path로 접근할 경우 상품 목록 조회에 실패한다.")
     void getProductsExceptionTest() {
-        ExtractableResponse<Response> response = 상품_목록_요청("/peo");
+        var response = 상품_목록_요청("/peo");
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    @DisplayName("상품을 생성한다.")
+    void postProduct() {
+        var response = 상품_생성_요청("햄버거", "https://www.mcdonalds.co.kr/upload/product/pcList/1683098039703.png", "5000");
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
 }
--- src/test/java/cart/acceptance/ProductSteps.java
+++ src/test/java/cart/acceptance/ProductSteps.java
@@ -5,6 +5,9 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.springframework.http.MediaType;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductSteps {
 
     public static ExtractableResponse<Response> 상품_목록_요청(String path) {
@@ -12,6 +15,20 @@ public class ProductSteps {
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .get(path)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 상품_생성_요청(String name, String image, String price) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("image", image);
+        params.put("price", price);
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/products")
                 .then().log().all()
                 .extract();
     }
