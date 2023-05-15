048eb7c4d516ed1c4f4e2238524139e7e3ae243e
jisu <jisuuoo96@gmail.com>
2023-05-15 20:52:11 +0900

    test: 상품 수정 기능 인수테스트 작성

--- src/test/java/cart/acceptance/ProductAcceptanceTest.java
+++ src/test/java/cart/acceptance/ProductAcceptanceTest.java
@@ -1,23 +1,26 @@
 package cart.acceptance;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import cart.controller.dto.ProductEditRequest;
+import cart.controller.dto.ProductRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-
-import static cart.acceptance.ProductSteps.상품_생성_요청;
+import static cart.acceptance.ProductSteps.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static cart.acceptance.ProductSteps.상품_목록_요청;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DisplayName("상품 관련 기능 인수테스트")
 class ProductAcceptanceTest {
 
+    private final ProductRequest createRequest = new ProductRequest("햄버거", "https://www.mcdonalds.co.kr/upload/product/pcList/1683098039703.png", 5000);
+    private final ProductEditRequest editRequest = new ProductEditRequest("햄버거", "https://www.mcdonalds.co.kr/upload/product/pcList/1683098039703.png", 10000);
+
     @Test
     @DisplayName("/products 로 접근할 경우 상품 목록 조회에 성공한다.")
     void getProductsTest() {
+
         var response = 상품_목록_요청("/products");
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
@@ -25,6 +28,7 @@ class ProductAcceptanceTest {
     @Test
     @DisplayName("/products 와 다른 path로 접근할 경우 상품 목록 조회에 실패한다.")
     void getProductsExceptionTest() {
+
         var response = 상품_목록_요청("/peo");
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
@@ -32,8 +36,33 @@ class ProductAcceptanceTest {
     @Test
     @DisplayName("상품을 생성한다.")
     void postProduct() {
-        var response = 상품_생성_요청("햄버거", "https://www.mcdonalds.co.kr/upload/product/pcList/1683098039703.png", "5000");
+
+        var response = 상품_생성_요청(createRequest);
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
+    @Test
+    @DisplayName("상품을 수정한다.")
+    void editProduct() {
+
+        var id = 상품_생성_요청(createRequest).jsonPath().getLong("id");
+
+        var response = 상품_수정_요청(id, editRequest);
+
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(상품_조회_단건_요청(id).jsonPath().getString("name")).isEqualTo(editRequest.getName()),
+                () -> assertThat(상품_조회_단건_요청(id).jsonPath().getString("image")).isEqualTo(editRequest.getImage()),
+                () -> assertThat(상품_조회_단건_요청(id).jsonPath().getInt("price")).isEqualTo(editRequest.getPrice())
+                );
+    }
+
+    @Test
+    @DisplayName("수정하려는 상품이 존재하지 않으면 수정에 실패한다.")
+    void editProductException() {
+
+        var response = 상품_수정_요청(3L, editRequest);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
 }
--- src/test/java/cart/acceptance/ProductSteps.java
+++ src/test/java/cart/acceptance/ProductSteps.java
@@ -1,5 +1,7 @@
 package cart.acceptance;
 
+import cart.controller.dto.ProductEditRequest;
+import cart.controller.dto.ProductRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
@@ -10,6 +12,15 @@ import java.util.Map;
 
 public class ProductSteps {
 
+    public static ExtractableResponse<Response> 상품_조회_단건_요청(Long productId) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/products/{productId}", productId)
+                .then().log().all()
+                .extract();
+    }
+
     public static ExtractableResponse<Response> 상품_목록_요청(String path) {
         return RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -19,16 +30,30 @@ public class ProductSteps {
                 .extract();
     }
 
-    public static ExtractableResponse<Response> 상품_생성_요청(String name, String image, String price) {
+    public static ExtractableResponse<Response> 상품_생성_요청(ProductRequest productRequest) {
         Map<String, String> params = new HashMap<>();
-        params.put("name", name);
-        params.put("image", image);
-        params.put("price", price);
+        params.put("name", productRequest.getName());
+        params.put("image", productRequest.getImage());
+        params.put("price", String.valueOf(productRequest.getPrice()));
         return RestAssured.given().log().all()
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .post("/products")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 상품_수정_요청(Long productId, ProductEditRequest productEditRequest) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", productEditRequest.getName());
+        params.put("image", productEditRequest.getImage());
+        params.put("price", String.valueOf(productEditRequest.getPrice()));
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .patch("/products/{productId}", productId)
                 .then().log().all()
                 .extract();
     }
