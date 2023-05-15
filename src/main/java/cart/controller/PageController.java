6349a266bb7983fe0533db662cb2feec63b65659
jisu <jisuuoo96@gmail.com>
2023-05-16 00:27:35 +0900

    feat: 관리자 도구 페이지 연동 구현

--- src/main/java/cart/controller/PageController.java
+++ src/main/java/cart/controller/PageController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.util.List;
 
 @Controller
@@ -20,5 +19,12 @@ public class PageController {
         List<Product> products = productService.findAll();
         model.addAttribute("products", products);
         return "index";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        List<Product> products = productService.findAll();
+        model.addAttribute("products", products);
+        return "admin";
     }
 }
