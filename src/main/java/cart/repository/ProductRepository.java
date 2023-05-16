d5025cff022a10fabd12e83ad69924d0d134cd7a
Yonghwi Kim <yh0921k@gmail.com>
2023-05-17 00:47:40 +0900

    feat: 상품 수정 쿼리 구현
    ProductRepository.java
    - 특정 id의 상품에 대해 전체 필드 업데이트

--- src/main/java/cart/repository/ProductRepository.java
+++ src/main/java/cart/repository/ProductRepository.java
@@ -41,4 +41,19 @@ public class ProductRepository {
         "SELECT P.ID AS id, P.NAME AS name, P.PRICE AS price, P.IMAGE_URL AS imageUrl FROM PRODUCT as P";
     return jdbcTemplate.query(query, BeanPropertyRowMapper.newInstance(Product.class));
   }
+
+  public void update(Long id, String name, int price, String imageUrl) {
+    String query = "UPDATE PRODUCT SET NAME = ?, PRICE = ?, IMAGE_URL = ? where ID = ?";
+    jdbcTemplate.update(
+        connection -> {
+          PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+          preparedStatement.setString(1, name);
+          preparedStatement.setInt(2, price);
+          preparedStatement.setString(3, imageUrl);
+          preparedStatement.setLong(4, id);
+
+          return preparedStatement;
+        });
+  }
 }
