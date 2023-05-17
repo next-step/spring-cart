c227311237a10bc1bd104248d97c88157680073b
chacha <peony@bluehole.net>
2023-05-17 22:44:02 +0900

    feat resource추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,14 @@
+DROP TABLE IF EXISTS PRODUCT;
 CREATE TABLE PRODUCT
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    image VARCHAR(200) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    PRIMARY KEY(id)
+    id    INT            NOT NULL AUTO_INCREMENT,
+    name  VARCHAR(50)    NOT NULL,
+    image VARCHAR(200)   NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (id)
 );
+INSERT INTO product(name, image, price)
+VALUES ('떡볶이', 'http://localhost:8080/images/dduckbbockcki.png', 10000),
+       ('김치찜', 'http://localhost:8080/images/kimchizzim.png', 20000),
+       ('해물찜', 'http://localhost:8080/images/seafoodzzim.png', 30000);
+
