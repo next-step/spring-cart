34900c740490ca0e3f898c68f8c27385ab9eba94
Yonghwi Kim <yh0921k@gmail.com>
2023-05-17 09:36:54 +0900

    feat: Admin 페이지 상품 목록 출력 연동
    CartViewController.java
    - 클래스명 변경
    - 기존 구현된 ProductService를 사용하여 현재 등록된 상품 목록 조회 및 페이지 연동
    
    admin.html
    - 컨트롤러에서 Model에 담아 내려준 데이터 매핑

--- src/main/java/cart/presentation/CartViewController.java
+++ src/main/java/cart/presentation/CartViewController.java
@@ -2,16 +2,15 @@ package cart.presentation;
 
 import cart.application.ProductService;
 import cart.application.dto.FindProductResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
 @RequiredArgsConstructor
-public class ProductViewController {
+public class CartViewController {
 
   private final ProductService productService;
 
@@ -21,5 +20,12 @@ public class ProductViewController {
     model.addAttribute("products", products);
 
     return "index";
+  }
+
+  @GetMapping("/admin")
+  public String getAdminPage(Model model) {
+    List<FindProductResponse> products = this.productService.getAllProducts();
+    model.addAttribute("products", products);
+    return "admin";
   }
 }
