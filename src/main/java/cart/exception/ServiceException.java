423cb33dea520be4afaff781d80a65c54f7b6a18
chacha <peony@bluehole.net>
2023-05-17 22:39:27 +0900

    refactor ErrorType Enum으로 변경

--- src/main/java/cart/exception/ServiceException.java
+++ src/main/java/cart/exception/ServiceException.java
@@ -1,14 +1,15 @@
 package cart.exception;
 
-public class ServiceException extends RuntimeException {
 
+public class ServiceException extends RuntimeException {
 	private final ErrorType errorType;
 
 	public ServiceException(ErrorType errorType) {
 		this.errorType = errorType;
 	}
 
-	public String errorCode() {
-		return this.errorType.name();
+	public ErrorType getErrorType() {
+		return errorType;
 	}
 }
+
