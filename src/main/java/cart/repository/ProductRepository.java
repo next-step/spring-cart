4fa989030c169b7689fb48ce8d1d3cafc6f89ef1
haileykim2014 <haileykim2014@gmail.com>
2023-05-29 12:22:05 +0900

    refactor : edit ProductRepositoryTest

--- src/main/java/cart/repository/ProductRepository.java
+++ src/main/java/cart/repository/ProductRepository.java
@@ -2,15 +2,26 @@ package cart.repository;
 
 import cart.domain.Product;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
+import javax.sql.DataSource;
+import org.apache.catalina.realm.DataSourceRealm;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
-@RequiredArgsConstructor
 @Repository
 public class ProductRepository {
 
   private final JdbcTemplate jdbcTemplate;
+  private final SimpleJdbcInsert jdbcInsert;
+
+  public ProductRepository(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+    this.jdbcTemplate = jdbcTemplate;
+    this.jdbcInsert = new SimpleJdbcInsert(dataSource)
+        .withTableName("product")
+        .usingGeneratedKeyColumns("id");
+  }
 
   public List<Product> getAll() {
     String sql = "SELECT * FROM product";
@@ -23,10 +34,11 @@ public class ProductRepository {
             .build());
   }
 
-  public void update(Product product) {
-    String sql = "UPDATE product SET name = ?, price = ?, image = ? WHERE id = ?";
-    jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImage(),
-        product.getId());
+  public Long update(Product product) {
+    String sql = "UPDATE product SET name = ?, image = ?, price = ? WHERE id = ?";
+
+    jdbcTemplate.update(sql, product.getName(), product.getImage(), product.getPrice(), product.getId());
+    return product.getId();
   }
 
   public Product findById(Long id) {
@@ -45,8 +57,15 @@ public class ProductRepository {
     jdbcTemplate.update(sql, id);
   }
 
-  public void save(Product entity) {
-    String sql = "INSERT INTO product (name, image, price) VALUES (?, ?, ?)";
-    jdbcTemplate.update(sql, entity.getName(), entity.getImage(), entity.getPrice());
+  public Product save(Product product) {
+    SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(product);
+    long id = jdbcInsert.executeAndReturnKey(parameterSource).longValue();
+
+    return Product.builder()
+        .id(id)
+        .name(product.getName())
+        .image(product.getImage())
+        .price(product.getPrice())
+        .build();
   }
 }
